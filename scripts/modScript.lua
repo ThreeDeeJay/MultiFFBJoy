0c2005396ae0e6af5ffa1fb8d5782ba6adfd2f25
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-17 18:39:32 -0400

    Add LUA script auto-start

---
+++ scripts/modScript.lua
@@ -0,0 +1,7 @@
+-- MultiFFBJoy startup script
+--
+-- This file is loaded by BeamNG at game startup.
+-- Keep the GE extension alive across level changes.
+
+setExtensionUnloadMode("multiffbjoy", "manual")
+loadManualUnloadExtensions()
