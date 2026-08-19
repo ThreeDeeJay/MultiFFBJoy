4c831eceb24e3e513488b6db976e4341dc2aba93
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-19 13:27:34 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@ namespace MultiFFBJoy
     std::mutex g_stateMutex;
     DeviceState g_state;
     std::vector<DeviceCandidate> g_candidates;
-    std::vector<PresetInfo> g_availablePresets;
     PresetTestState g_presetTestState;
 }
 using namespace MultiFFBJoy;
