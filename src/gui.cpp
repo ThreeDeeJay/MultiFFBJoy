52545462e349877bc35a1b24fca9a30fcc654eb4
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-20 12:33:02 -0400

    Update gui.cpp

--- src/gui.cpp
+++ src/gui.cpp
@@ -420,7 +420,7 @@ namespace MultiFFBJoy
                         {
                             Log(
                                 "Forcefield preset loaded.");
-                            UpdatePresetTest();
+                            StartPresetTest();
                         }
                         return 0;
                     }
