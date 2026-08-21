cc0165790f7f6e8eb95366323f939eaea8f44b19
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-08-21 18:46:12 -0400

    Update ffb.cpp

--- src/ffb.cpp
+++ src/ffb.cpp
@@ -412,15 +412,12 @@ namespace MultiFFBJoy
                 DI_FFNOMINALMAX);
         Logf(
             "Spring mapping \"%s\": "
-            "offset=(%ld,%ld), power=(%ld,%ld), "
-            "coeff+X=%ld coeff-X=%ld ",
+            "offset=(%ld,%ld), power=(%ld,%ld), ",
             forceField.name.c_str(),
             forceField.offsetX,
             forceField.offsetY,
             forceField.powerX,
-            forceField.powerY,
-            positiveCoefficientX,
-            negativeCoefficientX);
+            forceField.powerY);
         DICONDITION conditions[2]{};
         conditions[0].lOffset =
         springCenterX;
